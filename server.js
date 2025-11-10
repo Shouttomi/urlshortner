3bf1773514ec0536838e8bf17b8bcf8e1c69869c
Shouttomi <omiathak2002@gmail.com>
2025-11-10 17:24:38 +0530

    Added project files

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const  mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl')
+
+const app = express();
+
+
+
+mongoose.connect("mongodb+srv://omiathak:AThak240695%40%24@cluster0.yavsvcf.mongodb.net/urlshortner")
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch(err => console.error("MongoDB connection error:", err));
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}))
+
+app.get('/' ,async (req,res)=>{
+    const shortUrls = await ShortUrl.find()
+
+    console.log(shortUrls)
+
+    res.render('index', {shortUrls: shortUrls})
+   
+})
+
+app.post('/shortUrls', async (req,res)=> {
+await ShortUrl.create({full :req.body.fullUrl})
+
+res.redirect('/');
+})
+
+app.get('/:shortUrl', async (req,res)=> {
+
+    const shortUrl = await ShortUrl.findOne({short: req.params.shortUrl})
+
+    if(shortUrl ==null){
+        return res.sendStatus(404);
+    }
+    shortUrl.clicks++;
+    shortUrl.save();
+
+    res.redirect(shortUrl.full)
+})
+
+app.listen(process.env.PORT || 5000);
+
